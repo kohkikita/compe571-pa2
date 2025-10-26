843c26aa8d04fd07a7df8e9ffca47f9fe3c367e3
kohkikita <130106015+kohkikita@users.noreply.github.com>
2025-10-26 16:41:05 -0700

    created files for each method

---
+++ sjf.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <sys/wait.h> 
+#include <string.h> 
+#include <time.h> 
+#include <signal.h>
+#include <sys/time.h>
